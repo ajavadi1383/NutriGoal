8435b1bc9dcebcd90b5024f589c610ef56962640
Amirali Javadi <ajavadi335@yahoo.com>
2025-06-07 00:19:50 -0700

    Initial Commit

---
+++ NutriGoalTests/NutriGoalTests.swift
@@ -0,0 +1,17 @@
+//
+//  NutriGoalTests.swift
+//  NutriGoalTests
+//
+//  Created by Amirali Javadi on 6/7/25.
+//
+
+import Testing
+@testable import NutriGoal
+
+struct NutriGoalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
